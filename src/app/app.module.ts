8133fb67c4747b9f4d243ba4918c85417de3be25
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-11-24 15:59:08 +0300

    add component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { CategoryMenuComponent } from './category-menu/category-menu.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { ProjectComponent } from './project/project.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent,
+    CategoryMenuComponent,
+    TaskListComponent,
+    ProjectComponent,
+    ProjectListComponent
   ],
   imports: [
     BrowserModule,
