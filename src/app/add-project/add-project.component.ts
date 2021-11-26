983cbac831526c4802c737976a4523f84f918997
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-11-26 11:38:27 +0300

    project adding task is complete.

--- src/app/add-project/add-project.component.ts
+++ src/app/add-project/add-project.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Category } from '../models/category.model';
+import { Project } from '../models/project.model';
+import { CategoryService } from '../services/category.service';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-add-project',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoryService: CategoryService, private projectService : ProjectService) { }
 
   ngOnInit(): void {
+    this.categoryService.getCategories()
+      .subscribe((data: Category[]) => this.categories = data);
+  }
+
+  project: Project = new Project();
+  categories: Category[];
+
+  addProject(form: NgForm) {
+
+    console.log(form);
+    if (form.valid) {
+      console.log('dikkat:',form.value);
+      this.project = form.value;
+      console.log('eklenecek proje:',this.project)
+       this.projectService.addProject(this.project).subscribe((x:Project) => alert(x.name + " eklendi") );
+    }
+
+   
   }
 
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
 import { Project } from '../models/project.model';
@@ -21,6 +21,21 @@ export class ProjectService {
     return this.httpClient.get<Project[]>(this.url+'/'+id);
                           
   } 
+
+  addProject(project:Project):Observable<Project>{
+
+    /*
+    güvenlik söz konusu olduğunda, aşağıdaki gibi bir header bilgisi ile post edilmeli.
+    */
+       let option = {
+      headers: new HttpHeaders({
+        'Content-Type':'application/json',
+        'Authorization': 'Bearer [JWT TOKEN]'
+      })
+    };
+
+    return this.httpClient.post(this.url,project);
+  }
 
   // addProjects(project:Project):Observable<Project>{
   //   return this.httpClient.post("https://run.mocky.io/v3/ebbf0cde-f472-400e-b33e-6c9b45193124",project);
